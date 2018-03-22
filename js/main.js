bede963e2959079a1b8cf7a56679cd6027d3212e
Ryan Cain <racain05@gmail.com>
2018-03-22 13:19:18 -0400

    alignment styles modified for send email

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-// Creates reference to read more CTA button
-const readMoreBtn = document.querySelector('#read-more-btn')
-
-// Listens for click event on the read more button
-readMoreBtn.addEventListener('click', e => {
-    // Performs action upon click
-})
