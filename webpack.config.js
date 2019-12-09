230ee049fbe3b2f6fb3808ac3d8128f6f23c4cb1
Ryan Cain <racain05@gmail.com>
2019-12-09 02:00:48 -0500

    updated with patch v2.1.1javascript-serializer

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
+// SASS compiler to CSS
 module.exports = {
-  entry: './src/app.js',
+  mode: 'production',
+  entry: {
+    app: ['./src/styles/main.scss']
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'app.bundle.js'
-  }
-};
+  },
+    module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'},
+        ],
+        exclude: path.resolve(__dirname, '/node_modules/')
+      },
+    ],
+  },
+}
