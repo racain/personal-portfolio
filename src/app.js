4f7aa26163f519117c98aed129d94eefc892045a
Ryan Cain <racain05@gmail.com>
2020-03-31 16:15:18 -0400

    made changes to app.js and styles/main.scss

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import './styles/main.scss';
-import sayHello from './js/sayHello';
-
-sayHello('Ryan');
-=======
 import './styles/normalize.scss'
 import './styles/main.scss';
 import './js/components/MessageFlash';
->>>>>>> develop
